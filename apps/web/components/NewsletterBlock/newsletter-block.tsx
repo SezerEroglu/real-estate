0826e350500bd6f20ca4adceb147ea0c60024346
SezerEroglu <sezer.eroglu@amplicade.com>
2025-06-19 17:23:47 +0300

    <feat> newsletter background update

--- apps/web/components/NewsletterBlock/newsletter-block.tsx
+++ apps/web/components/NewsletterBlock/newsletter-block.tsx
@@ -6,10 +6,9 @@ export default function NewsletterBlock() {
       id="newsletter"
       className="container mx-auto my-16 w-full scroll-mt-40 md:scroll-mt-20 lg:scroll-mt-16"
     >
-      <div className="to-primary1 text-primary1-foreground relative grid min-h-[820px] overflow-hidden rounded-3xl bg-gradient-to-b from-transparent transition-background lg:grid-cols-2 lg:bg-gradient-to-r">
-        <div
-          className={'absolute bottom-0 left-0 right-0 top-0 overflow-hidden'}
-        />
+      <div className="text-primary1-foreground relative grid min-h-[820px] overflow-hidden rounded-3xl bg-[url('/images/newsletter-bg.jpg')] bg-contain lg:grid-cols-2">
+        <div className="absolute inset-0 overflow-hidden bg-[linear-gradient(to_bottom,rgba(0,0,0,0)_0%,hsl(var(--heroui-primary2)/0.3)_25%,hsl(var(--heroui-primary2)/1)_30%)] transition-background md:bg-[linear-gradient(to_bottom,rgba(0,0,0,0)_0%,hsl(var(--heroui-primary2)/0.3)_35%,hsl(var(--heroui-primary2)/1)_40%)] lg:bg-[linear-gradient(to_right,rgba(0,0,0,0)_0%,hsl(var(--heroui-primary2)/1)_60%,hsl(var(--heroui-primary2)/1)_100%)]" />
+
         <div />
         <div className="z-20 mt-[65vw] flex items-center justify-center md:mt-[400px] md:p-20 lg:mt-0">
           <div className="bg-primary2 text-primary2-foreground m-4 w-full rounded-2xl border-divider/20 p-4 md:p-10 lg:rounded-[20px] lg:border">
