6c53a77b6e8fdbdd7cb5095c5038c6afd75e2880
SezerEroglu <sezer.eroglu@amplicade.com>
2025-06-19 16:41:20 +0300

    <fix> newsletter padding fix

--- apps/web/components/NewsletterBlock/newsletter-block.tsx
+++ apps/web/components/NewsletterBlock/newsletter-block.tsx
@@ -4,7 +4,7 @@ export default function NewsletterBlock() {
   return (
     <div
       id="newsletter"
-      className="container mx-auto my-16 w-full scroll-mt-40 px-4 md:scroll-mt-20 lg:scroll-mt-16"
+      className="container mx-auto my-16 w-full scroll-mt-40 md:scroll-mt-20 lg:scroll-mt-16"
     >
       <div className="to-primary1 text-primary1-foreground relative grid min-h-[820px] overflow-hidden rounded-3xl bg-gradient-to-b from-transparent transition-background lg:grid-cols-2 lg:bg-gradient-to-r">
         <div
